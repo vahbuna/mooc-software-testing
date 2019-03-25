4aa9be806caac4f03d0b50742c52a193467bf3a8
vahbuna <silver.estla@gmail.com>
2019-03-25 12:11:24 +0100

    topic 2 quiz

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,36 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+        
 public class CaesarShiftCipherTest {
+    private CaesarShiftCipher cipher;
+    
+    @BeforeEach
+    public void setupObject() {
+        cipher = new CaesarShiftCipher();
+    }
+    
+    @Test
+    public void smallJump() {
+        Assertions.assertEquals("fghi", cipher.CaesarShiftCipher("abcd", 5));
+    
+    }
+    
+    @Test
+    public void noJump() {
+        Assertions.assertEquals("temp", cipher.CaesarShiftCipher("temp", 0));
+    }
+    
+    @Test
+    public void smallReverseJump() {
+        Assertions.assertEquals("abcd", cipher.CaesarShiftCipher("fghi", -5));
+    }
+    
+    @Test
+    public void cycle() {
+        Assertions.assertEquals("temp", cipher.CaesarShiftCipher("temp", 26));
+    }
 
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,14 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void defaultTests() {
+        GHappy happy = new GHappy();
+        Assertions.assertTrue(happy.gHappy("xxggxx"));
+        Assertions.assertFalse(happy.gHappy("xxgxx"));
+        Assertions.assertFalse(happy.gHappy("xxggyygxx"));
+    }
 }
