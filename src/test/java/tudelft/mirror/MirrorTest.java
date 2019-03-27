b9098c1fcb88eec1c08326d1450d78c56aed5c55
vahbuna <silver.estla@gmail.com>
2019-03-27 07:40:29 +0100

    topic 5 quiz

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,20 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.*;
+
 public class MirrorTest {
+    private Mirror mirror;
+
+    @BeforeEach
+    public void init() {
+        mirror = new Mirror();
+    }
+
+    @Test
+    public void mirrorTests() {
+        Assertions.assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+        Assertions.assertEquals("a", mirror.mirrorEnds("abca"));
+        Assertions.assertEquals("aba", mirror.mirrorEnds("aba"));
+        Assertions.assertEquals("abba", mirror.mirrorEnds("abba"));
+    }
 }
